abc4ea727a61dc243e425f65bf51d086f26c2935
Andrew Jennings <andrew_jennings@ymail.com>
2017-06-20 07:27:10 +0200

    completed prob1 and tested

--- prob1.py
+++ prob1.py
@@ -1,1 +1,10 @@
 #prob1
+
+balance = 42
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+
+for i in range(12):
+    unpaidBalance = balance - (balance*monthlyPaymentRate)
+    balance = unpaidBalance + (unpaidBalance * (annualInterestRate/12))
+print('Remaining balance: ' + str(round(balance, 2)))
