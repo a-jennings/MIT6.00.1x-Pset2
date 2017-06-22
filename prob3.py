ce9c778b7fe3eb659f6d2243bfe0719213853255
Andrew Jennings <andrew_jennings@ymail.com>
2017-06-22 07:31:49 +0200

    prob3 no longer recursive due to stack issues, works but no bisectional search as required

--- prob3.py
+++ prob3.py
@@ -1,1 +1,29 @@
-#prob3
+balance = 320000
+annualInterestRate = 0.2
+#Result 29157.09
+
+
+
+def lowpay(balance, annualInterestRate):
+    currBalance = balance
+    fixedPay = 0
+    
+    while balance > 0:
+        currBalance = balance
+        for i in range(12):
+            balance = balance - fixedPay
+            balance = balance + ((balance*annualInterestRate/12))
+        if balance > 0:
+            fixedPay += 0.01
+            balance = currBalance
+        else:
+            return round(fixedPay, 2)
+
+
+#This currently works but has no bisect search which is required
+
+
+
+
+
+print("Lowest payment: " + str(lowpay(balance, annualInterestRate)))
