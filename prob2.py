3f9caa0b80dd9567d58b14af31237686cae5c0dc
Andrew Jennings <andrew_jennings@ymail.com>
2017-06-22 06:50:20 +0200

    prob2 completed and tested

--- prob2.py
+++ prob2.py
@@ -1,1 +1,20 @@
-#prob2
+balance = 3926
+annualInterestRate = 0.2
+fixedPay = 360
+#Result 360
+
+def lowpay(balance, annualInterestRate, fixedPay):
+    
+    while balance > 0:
+        currBalance = balance
+        for i in range(12):
+            balance = balance - fixedPay
+            balance = balance + ((balance*annualInterestRate/12))
+        if balance > 0:
+            return lowpay(currBalance, annualInterestRate, fixedPay+10)
+        else:
+            return fixedPay
+
+
+print("Lowest payment: " + str(lowpay(balance, annualInterestRate, fixedPay)))
+
